bfc4b8583257d144725b4da5e3c631902fbdada3
Lorenzo Bucciarelli <lorenzo.bucciarelli@hotmail.it>
2017-11-15 15:38:31 -0800

    feta: Added a new TextView

---
+++ app/src/androidTest/java/com/example/android/justjava/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.android.justjava;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lorenzo Bucciarelli on 11/14/2017.
+ */
+public class MainActivityTest {
+    @Test
+    public void submitOrder() throws Exception {
+    }
+
+}
---
+++ app/src/test/java/com/example/android/justjava/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.android.justjava;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lorenzo Bucciarelli on 11/14/2017.
+ */
+public class MainActivityTest {
+
+}
